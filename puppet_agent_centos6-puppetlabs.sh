998968fcf6b81d8cc8bf3ae4dbf76727071ae0ed
gtpuppet2014 <gtpuppet2014@gmail.com>
2014-03-24 20:13:22 +0100

    changed puppet_agent_centos6 script

--- puppet_agent_centos6-puppetlabs.sh
+++ puppet_agent_centos6-puppetlabs.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-# Basado en: # https://raw.github.com/hashicorp/puppet-bootstrap/master/centos_6_x.sh
+# Basado en: https://raw.github.com/hashicorp/puppet-bootstrap/master/centos_6_x.sh
 
 REPO_URL="http://yum.puppetlabs.com/el/6/products/i386/puppetlabs-release-6-7.noarch.rpm		"
 PUPPETLABS_REPO="http://yum.puppetlabs.com"
-#http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
 REDHAT_RELEASE="/etc/redhat-release"
 EPEL_REPO="http://dl.fedoraproject.org/pub/epel"
 
@@ -18,7 +17,7 @@ setenforce permissive
 
 # Paqueteria recomendable ou necesaria
 yum update --yes
-yum install wget screen telnet redhat-lsb
+yum install -y wget screen telnet redhat-lsb
 yum groupinstall -y "Development Tools"
 
 if which puppet > /dev/null 2>&1; then
@@ -27,19 +26,18 @@ if which puppet > /dev/null 2>&1; then
 else
 # PuppetLabs repo
   cd /tmp
+#  http://docs.puppetlabs.com/guides/puppetlabs_package_repositories.html#for-red-hat-enterprise-linux-and-derivatives
 #  wget http://yum.puppetlabs.com/el/6/products/x86_64/puppetlabs-release-6-10.noarch.rpm
 #  rpm -ivh puppetlabs-release-6-10.noarch.rpm
-#
-# http://docs.puppetlabs.com/guides/puppetlabs_package_repositories.html#for-red-hat-enterprise-linux-and-derivatives
-  rpm -ivh https://yum.puppetlabs.com/el/6/products/x86_64/puppetlabs-release-6-10.noarch.rpm
+  rpm -ivh https://yum.puppetlabs.com/el/6/products/x86_64/puppetlabs-release-6-7.noarch.rpm
   yum -y update
 
 # Puppet (ultima version no repositorio)
 #  yum install -y puppet facter
 
 # facter 1.7.4 e puppet 3.4.2
-  yum -y install facter-1.7.4-1.el6.x86_64.rpm
-  yum -y install puppet-3.4.2-1.el6.x86_64.rpm
+  yum -y install facter-1.7.4-1.el6.noarch
+  yum -y install puppet-3.4.2-1.el6.noarch
 fi
 
 # Puppet agent en modo 'daemon'	
